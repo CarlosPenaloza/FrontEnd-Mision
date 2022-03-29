1fb42cbac37cf3a5d429c2b76907759c54d2d8c9
Chars Eine <carlosPenaloza>
2022-03-29 13:44:21 -0600

    Se modificaron las clases del HTML de la seccion "CSS"

---
+++ FrontEnd-Mision-main/03 - CSS/practicas-sin-web-pack/src/ts/dom.ts
@@ -0,0 +1,13 @@
+const htmlSvgUp: string = `
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-corner-right-up" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6 18h6a3 3 0 0 0 3 -3v-10l-4 4m8 0l-4 -4" />
+  </svg>
+`;
+
+const htmlSvgDown: string = `
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-corner-right-down" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M6 6h6a3 3 0 0 1 3 3v10l-4 -4m8 0l-4 4" />
+  </svg>
+`;
